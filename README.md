60aa02f924592db9e2de1c7f43ea20d931485a88
Dexter Chan <dexterchann@gmail.com>
2022-11-18 12:07:11 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# TODO
+
+## Server
+1. Setup Endpoint
+1. Setup Typescript
+1. Setup Linting and autoformatting
+1. Middleware?
+
+## Client
+1. Setup Next.js app
+1. Setup Typescript
+1. Setup Linting and autoformatting
---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = 8000
+
+const RECIPES = [
+  {
+    id: 1,
+    title: "Mashed Potatoes"
+  },
+  {
+    id: 2,
+    title: "Roasted Potatoes"
+  }
+];
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get("/recipes", (req, res) => {
+  res.json(RECIPES);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
