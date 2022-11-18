489856f950899a2c932bb797c89c10761326ed43
Dexter Chan <dexterchann@gmail.com>
2022-11-18 12:13:41 -0800

    deinit sub git repo

---
+++ client/pages/recipes/index.tsx
@@ -0,0 +1,36 @@
+import { useQuery } from "react-query";
+
+async function getRecipes() {
+  const res = await fetch("http://localhost:8000/recipes");
+
+  if (!res.ok) {
+    throw new Error(res.statusText);
+  }
+
+  const data = await res.json();
+  return data;
+}
+
+export default function RecipesRoute() {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['recipes'],
+    queryFn: getRecipes
+  })
+
+  if (isLoading) return 'Loading...'
+  if (error) return 'An error has occurred: ' + error.message
+
+  return (
+    <>
+      <h1>Recipes</h1>
+      <ul>
+        {data.map((recipe) => {
+          return (
+            <li key={recipe.id}>{recipe.title}</li>
+          )
+        })}
+      </ul>
+    </>
+
+  )
+}
