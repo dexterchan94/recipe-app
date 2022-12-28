1901115204659d335cf84cba0c32f42b3f9e57e4
Dexter Chan <dexterchann@gmail.com>
2022-12-28 15:18:55 -0800

    schema draft

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,55 @@
-console.log('run server');
+import { readFileSync } from 'fs';
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+const recipes = [
+  {
+    id: '1',
+    title: 'Canned soup',
+    steps: [
+      {
+        body: 'Open can.',
+      },
+      {
+        body: 'Heat soup.',
+      },
+      {
+        body: 'Serve.',
+      },
+    ],
+  },
+  {
+    id: '2',
+    title: 'Toast',
+    steps: [
+      {
+        body: 'Toast bread.',
+      },
+      {
+        body: 'Spread butter and jam.',
+      },
+      {
+        body: 'Serve.',
+      },
+    ],
+  },
+];
+
+const typeDefs = readFileSync('./schema/schema.graphql', { encoding: 'utf-8' });
+
+const resolvers = {
+  Query: {
+    recipes: () => recipes,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
