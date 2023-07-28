261a6096f8d274f69e5bab7487cd464bc7da41b7
Dexter Chan <dexterchann@gmail.com>
2023-07-28 11:00:45 -0700

    fix linting autoformat

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -1,9 +1,5 @@
-import {
-  intArg,
-  makeSchema,
-  objectType,
-} from 'nexus'
-import { Context } from './context'
+import { intArg, makeSchema, objectType } from 'nexus';
+import { Context } from './context';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { createRequire } from 'node:module';
@@ -18,9 +14,9 @@ const Query = objectType({
     t.list.nonNull.field('allUsers', {
       type: 'User',
       resolve: (_parent, _args, context: Context) => {
-        return context.prisma.user.findMany()
+        return context.prisma.user.findMany();
       },
-    })
+    });
 
     t.nullable.field('userById', {
       type: 'User',
@@ -30,16 +26,16 @@ const Query = objectType({
       resolve: (_parent, args, context: Context) => {
         return context.prisma.user.findUnique({
           where: { id: args.id || undefined },
-        })
+        });
       },
-    })
+    });
 
     t.list.nonNull.field('allRecipes', {
       type: 'Recipe',
       resolve: (_parent, _args, context: Context) => {
-        return context.prisma.recipe.findMany()
+        return context.prisma.recipe.findMany();
       },
-    })
+    });
 
     t.nullable.field('recipeById', {
       type: 'Recipe',
@@ -49,18 +45,18 @@ const Query = objectType({
       resolve: (_parent, args, context: Context) => {
         return context.prisma.recipe.findUnique({
           where: { id: args.id || undefined },
-        })
+        });
       },
-    })
+    });
   },
-})
+});
 
 const User = objectType({
   name: 'User',
   definition(t) {
-    t.nonNull.int('id')
-    t.string('name')
-    t.nonNull.string('email')
+    t.nonNull.int('id');
+    t.string('name');
+    t.nonNull.string('email');
     t.list.nonNull.field('recipes', {
       type: 'Recipe',
       resolve: (parent, _, context: Context) => {
@@ -68,27 +64,23 @@ const User = objectType({
           .findUnique({
             where: { id: parent.id || undefined },
           })
-          .recipes()
+          .recipes();
       },
-    })
+    });
   },
-})
+});
 
 const Recipe = objectType({
-  name: "Recipe",
+  name: 'Recipe',
   definition(t) {
-    t.nonNull.int('id')
-    t.nonNull.string('title')
-    t.nonNull.int('authorId')
-  }
-})
+    t.nonNull.int('id');
+    t.nonNull.string('title');
+    t.nonNull.int('authorId');
+  },
+});
 
 export const schema = makeSchema({
-  types: [
-    Query,
-    Recipe,
-    User,
-  ],
+  types: [Query, Recipe, User],
   outputs: {
     schema: __dirname + '/../schema.graphql',
     typegen: __dirname + '/generated/nexus.ts',
@@ -105,4 +97,4 @@ export const schema = makeSchema({
       },
     ],
   },
-})
+});
