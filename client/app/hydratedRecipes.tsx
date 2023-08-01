87511b412c6d66fe0e57c438f8369977964d0698
Dexter Chan <60243601+dexterchan94@users.noreply.github.com>
2023-08-01 11:55:51 -0700

    Add client github action (#8)
    * add dependency caching in workflow
    
    * setup action
    
    * add action file
    
    * disable cache for first run
    
    * re-enable cache
    
    * add path to lock file
    
    * add path to server workflow

--- client/app/hydratedRecipes.tsx
+++ client/app/hydratedRecipes.tsx
@@ -1,12 +1,12 @@
 import { dehydrate, Hydrate } from '@tanstack/react-query';
 import getQueryClient from './getQueryClient';
 import Recipes from './recipes';
-import { TestQueryDocument } from '../queries/generated';
+import { RecipesDocument } from '../queries/generated';
 import { fetcher } from '../queries/fetcher';
 
 export default async function HydratedRecipes() {
   const queryClient = getQueryClient();
-  await queryClient.prefetchQuery(['testQuery'], fetcher(TestQueryDocument));
+  await queryClient.prefetchQuery(['recipes'], fetcher(RecipesDocument));
   const dehydratedState = dehydrate(queryClient);
 
   return (
